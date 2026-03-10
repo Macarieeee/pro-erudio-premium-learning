678835abb5d4e9a5f1755f52f38ff53eeb392d34
Macarieeee <mihaimacarie113@gmail.com>
2026-03-10 12:21:37 +0200

    og jurnale tabere

--- src/pages/JurnaleTabara.tsx
+++ src/pages/JurnaleTabara.tsx
@@ -4,18 +4,49 @@ import { Card, CardContent } from "@/components/ui/card";
 
 // ✅ din fișierul tău journals.ts
 import { journalList } from "@/data/journals";
-
+import { Head } from "vite-react-ssg";
 // ✅ ajustează dacă la tine se numesc altfel / sunt în alt path
 import Navigation from "@/components/Navigation";
 import Footer from "@/components/Footer";
 
 const JurnaleTabara = () => {
+  const canonicalUrl = "https://tabere.proerudio.ro/jurnale";
   return (
     <>
+    <Head>
+        <title>Jurnale de Tabără | Pro Erudio</title>
+
+        <meta
+  name="description"
+  content="Descoperă jurnalele de tabără Pro Erudio și vezi activități, experiențe și momente reale din taberele noastre de engleză pentru copii și adolescenți."
+/>
+
+        <link rel="canonical" href={canonicalUrl} />
+
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content={canonicalUrl} />
+        <meta property="og:title" content="Jurnale de Tabără | Pro Erudio" />
+        <meta
+  property="og:description"
+  content="Vezi jurnalele de tabără Pro Erudio și descoperă experiențele copiilor și adolescenților din taberele noastre de engleză."
+/>
+        <meta property="og:image" content="https://tabere.proerudio.ro/og/Ziua5-1.jpg" />
+        <meta property="og:image:width" content="1200" />
+        <meta property="og:image:height" content="630" />
+
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="Despre noi – Tabere de Engleză Pro Erudio" />
+        <meta
+          name="twitter:description"
+          content="Povestea Pro Erudio, echipa și valorile noastre. Tabere de engleză în România și în străinătate."
+        />
+        <meta name="twitter:image" content="https://tabere.proerudio.ro/og/herohome.jpeg"  />
+      </Head>
       <Navigation />
 
       {/* Hero / intro */}
       <section className="pt-40 bg-background">
+        
         <div className="container mx-auto px-4 lg:px-8">
           <div className="max-w-4xl mx-auto text-center">
             <h1 className="text-4xl md:text-5xl font-bold text-foreground mb-4">
